c14e57bc54a322efc3e85068e4a8a4a2757a6ba8
CaDoCoda <dyhn55c6r4@privaterelay.appleid.com>
2026-06-27 15:32:19 -0600

    Delete Shellscript 1.bash

--- Shellscript 1.bash
+++
@@ -1,1 +0,0 @@
-sudo find / -type d -name "CoreAI.mlpackage" -exec sh -c 'xcrun coremlcompiler compile "$1" ./OutputFolder && swift run_coreml_model.swift ./OutputFolder/CoreAI.mlmodelc' _ {} \; 2>/dev/null
